3ffb4dc226d4013c57ab83fca8073d78ba03089a
jjcanon <javier.canon@outlook.com>
2026-05-29 11:52:24 -0500

    update UI to use Blazor components from Blazorise

--- src/RulesEngine.UI/Program.cs
+++ src/RulesEngine.UI/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RulesEngine.UI;
+using Blazorise;
+using Blazorise.Tailwind;
+using Blazorise.Icons.Lucide;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +14,10 @@ builder.Services.AddScoped(_ =>
     var backendUrl = builder.Configuration["BackendUrl"] ?? "https://localhost:7086";
     return new HttpClient { BaseAddress = new Uri(backendUrl) };
 });
+
+builder.Services
+    .AddBlazorise()
+    .AddTailwindProviders()
+    .AddLucideIcons();
 
 await builder.Build().RunAsync();
