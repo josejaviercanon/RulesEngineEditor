de2dd3d3fb991fb7b330918d4f148d573164ed41
jjcanon <javier.canon@outlook.com>
2026-05-30 14:44:54 -0500

    react solution setup

---
+++ src/RulesEngineEditor.Server/Program.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Alex Reich.
+// Licensed under the CC BY 4.0 License.
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
