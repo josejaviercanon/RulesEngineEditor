affaf864b2c182630d3320bdb69f6f36b646e3e6
jjcanon <88041834+josejaviercanon@users.noreply.github.com>
2026-06-07 18:49:02 -0500

    Remove obsolete files from the project

--- src/BE.Libraries/RulesEngine/src/RulesEngine/Models/RuleDelegate.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-namespace RulesEngine.Models
-{
-    public delegate T RuleFunc<T>(params RuleParameter[] ruleParameters);
-}
